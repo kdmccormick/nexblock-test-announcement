c907fc2e5db18d82b41cb9ef4f591ee6fac32bec
Jie Han <jhan@edx.org>
2021-02-10 12:20:35 -0500

    render react pages without needing to sign in

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,35 @@
 import 'babel-polyfill';
 
-import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
-} from '@edx/frontend-platform';
-import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+// import {
+//  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+// } from '@edx/frontend-platform';
+// import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
-import Header, { messages as headerMessages } from '@edx/frontend-component-header';
-import Footer, { messages as footerMessages } from '@edx/frontend-component-footer';
+// import Header, { messages as headerMessages } from '@edx/frontend-component-header';
+// import Footer, { messages as footerMessages } from '@edx/frontend-component-footer';
 
-import appMessages from './i18n';
 import ExamplePage from './example/ExamplePage';
+import Announcement from './ui/learner/Announcement';
+import NotFoundPage from './ui/NotFoundPage';
 
 import './index.scss';
 
-subscribe(APP_READY, () => {
-  ReactDOM.render(
-    <AppProvider>
-      <Header />
-      <ExamplePage />
-      <Footer />
-    </AppProvider>,
-    document.getElementById('root'),
-  );
-});
+const myStuff = (
+  <>
+    <Router>
+      <Switch>
+        <Route path="/example" component={ExamplePage} />
+        <Route path="/announcement" component={Announcement} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+    </Router>
+  </>
+);
 
-subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-});
-
-initialize({
-  messages: [
-    appMessages,
-    headerMessages,
-    footerMessages,
-  ],
-});
+ReactDOM.render(
+  myStuff,
+  document.getElementById('root'),
+);
---
+++ src/ui/learner/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Announcement() {
+  return (
+    <main>
+      <div className="container-fluid">
+        <h1>This is the annoucement</h1>
+        <p>Here is the dummy API data about the annoucement.</p>
+      </div>
+    </main>
+  );
+}

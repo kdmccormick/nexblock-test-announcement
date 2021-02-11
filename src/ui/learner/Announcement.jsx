8645d9ab2c08b4a13d39d83d64c1399fa6ccdf14
Talia Rhodes <trhodes@edx.org>
2021-02-11 13:55:41 -0500

    modal

--- src/ui/learner/Announcement.jsx
+++ src/ui/learner/Announcement.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Modal } from '@edx/paragon';
 
 import { getOrders } from './data';
 
@@ -9,6 +10,18 @@ class Announcement extends React.Component {
     this.state = {
       data: null,
     };
+    this.openModal = this.openModal.bind(this);
+    this.resetModalWrapperState = this.resetModalWrapperState.bind(this);
+
+    this.state = { open: true };
+  }
+
+  openModal() {
+    this.setState({ open: true });
+  }
+
+  resetModalWrapperState() {
+    this.setState({ open: false });
   }
 
   componentDidMount() {
@@ -16,26 +29,17 @@ class Announcement extends React.Component {
   }
 
   render() {
+    const { data, open } = this.state
+    let modal;
+    if (data) {
+      modal = <Modal open={open} title={data.title} body={data.body} onClose={() => {}} />
+    }
     return (
-      <main>
+      <>
         <div className="container-fluid">
-          <h1>This is the annoucement</h1>
-          <p>Here is the dummy API data about the annoucement.</p>
-          {
-            this.state.data !== null
-            && (this.state.data.map(
-              ({
-                name, courseId, start, end,
-              }) => (
-                <p key={courseId}>Course name: <b>{name}</b> ({courseId})
-                  <br />
-                  <i>Runs from {start} to {end || 'the end of time'}</i>
-                </p>
-              ),
-            ))
-          }
+          {modal}
         </div>
-      </main>
+      </>
     );
   }
 }
--- src/ui/learner/data.js
+++ src/ui/learner/data.js
@@ -2,27 +2,18 @@
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { camelCaseObject } from '@edx/frontend-platform';
 
-const DATA_URL = 'http://localhost:18000/api/courses/v1/courses/';
+// For testing. This would not normally be defined in here at all.
+const usageKey = "block-v1:edX+ART150+3T2020+type@nexblock+block@4ed0c8a2471f44bd83c0dcabc9b9b86e"
+
+const DATA_URL = `http://localhost:18000/api/nexblocks/v0/instance-data/${usageKey}`;
 
 // eslint-disable-next-line import/prefer-default-export
 export async function getOrders() {
   const httpClient = getAuthenticatedHttpClient();
   // const { username } = getAuthenticatedUser();
 
-  const transformedResults = (response) => {
-    const results = camelCaseObject(response.results);
-    return results.map(
-      ({
-        name, courseId, start, end,
-      }) => ({
-        name, courseId, start, end,
-      }),
-    ); // google es6 object destructuring
-  };
-
   const { data } = await httpClient.get(DATA_URL, {
     params: {},
   });
-
-  return { data: transformedResults(data) };
+  return { data: data };
 }
